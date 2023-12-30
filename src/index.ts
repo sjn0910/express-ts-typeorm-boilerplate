6817aacf85678709d4d1d248595f4a6610de62b5
sjn0910 <sjn0910@naver.com>
2023-12-30 13:03:56 +0900

    make custom error and error handler

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import express from 'express';
 import 'reflect-metadata';
 import AppDataSource from './config/dataSource';
 import './config/env';
+import errorHandler from './util/errorHandler';
 
 const PORT = Number(process.env.PORT) || 3000;
 
 const app = express();
 
+AppDataSource.initialize().then(() => console.log('DATABASE is connected!'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-AppDataSource.initialize().then(() => console.log('DATABASE is connected!'));
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is started!`));
